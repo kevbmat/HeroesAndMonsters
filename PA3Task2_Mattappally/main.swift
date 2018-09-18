4ad5ec1f073939f33e7a0c607544a7bfaa4517c9
Kevin Mattappally <kevinmat@outlook.com>
2018-09-18 10:30:14 -0700

    Initial commit

---
+++ PA3Task2_Mattappally/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PA3Task2_Mattappally
+//
+//  Created by Kevin Mattappally on 9/18/18.
+//  Copyright © 2018 Kevin Mattappally. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
